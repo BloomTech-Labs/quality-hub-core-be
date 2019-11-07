b43a55c1eb001a97149dc84d9f95e6edf8f295d3
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-11-07 17:33:27 -0500

    getUserId Middleware.

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
-export const JWT_SECRET = "QUAIL_LIFESTYLE"
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "QUAIL_LIFESTYLE"
+
+
+function getUserId(context) {
+    const Authorization = context.request.get('Authorization')
+    if(Authorization) {
+        const token = Authorization.replace('Bearer', '')
+        const { userId } = jwt.verify (token, JWT_SECRET)
+        return userId
+    }
+    throw new Error('Not Authenticated')
+}
+
+module.exports = {
+    JWT_SECRET,
+    getUserId,
+}
