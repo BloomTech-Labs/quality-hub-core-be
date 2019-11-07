a1604a96f73b7debe937db470e9e112ecf948541
Jordan <jordandoan@hotmail.com>
2019-11-06 22:27:42 -0800

    Initial server start for back end

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+const resolvers = {
+  Query: {
+    info: () => `Welcome to Quaility Hub!`
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
