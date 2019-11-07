6bd9649f42c01d56e573ea8a59e7a14ea8aa8f40
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-11-07 17:18:17 -0500

    version 1 of login functionality

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const { generateToken }= require('../utils')
+
 async function signup(parent, args, context, info) {
   
   const hash = bcrypt.hashSync(args.password, 10)
@@ -7,6 +10,27 @@ async function signup(parent, args, context, info) {
   return await context.prisma.createUser(args)
 }
 
+async function login(parent, args, context, info) {
+  console.log(args);
+  const user = await context.prisma.user(args.username)
+  if (!user) {
+    throw new Error('Invalid Login')
+  }
+
+  const passwordMatch = await bcrypt.compare(args.password, user.password)
+  if (!passwordMatch) {
+    throw new Error('Invalid Login')
+  }
+
+  const token = generateToken(user)
+
+  return {
+    token,
+    user,
+  }
+}
+
 module.exports = {
-  signup
+  signup,
+  login
 }
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
-export const JWT_SECRET = "QUAIL_LIFESTYLE"
+const JWT_SECRET = "QUAIL_LIFESTYLE"
+
+function generateToken(user) {
+    const payload = {
+        id: user.id,
+        email: user.email,
+    };
+    const options = {
+        expiresIn: '12h'
+    }
+    return jwt.sign(payload, JWT_SECRET, options)
+}
+
+  module.exports = {
+      generateToken,
+      JWT_SECRET,
+    }
