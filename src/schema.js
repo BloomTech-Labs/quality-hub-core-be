c52a82ef3d6c7876affcb4e3b1751998b81dbcf7
Jonathan Ho <johnnyho82@gmail.com>
2019-11-07 19:06:04 -0500

    Implement Apollo Federation

---
+++ src/schema.js
@@ -0,0 +1,50 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    first_name: String!
+    last_name: String!
+    email: String!
+    city: String!
+    state: String!
+    # industry: [String!]
+    image_url: String
+    gender: String
+    # resumes: [String]
+    personal_url: String
+    blog_url: String
+    twitter_url: String
+    portfolio_url: String
+    linkedin_url: String
+    github_url: String
+    bio: String
+    payment_info: Boolean
+  }
+
+  type Query {
+    info: String!
+    users: [User!]!
+    user(id: ID!): User!
+    me: User!
+  }
+
+  type Mutation {
+    signup(
+      first_name: String!
+      last_name: String!
+      email: String!
+      password: String!
+      city: String!
+      state: String!
+    ): AuthPayload!
+    login(email: String!, password: String!): AuthPayload!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+`;
+
+module.exports = typeDefs;
