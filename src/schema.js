c0a17c35ddba712fcd6f89d529c3b756f8aafb7e
Jonathan Ho <johnnyho82@gmail.com>
2019-11-07 19:08:34 -0500

    Update schema.js file

--- src/schema.js
+++ src/schema.js
@@ -39,6 +39,25 @@ const typeDefs = gql`
       state: String!
     ): AuthPayload!
     login(email: String!, password: String!): AuthPayload!
+    update(
+      first_name: String
+      last_name: String
+      email: String
+      city: String
+      state: String
+      # industry: [String]
+      image_url: String
+      gender: String
+      # resumes: [String]
+      personal_url: String
+      blog_url: String
+      twitter_url: String
+      portfolio_url: String
+      linkedin_url: String
+      github_url: String
+      bio: String
+      payment_info: Boolean
+    ): User!
   }
 
   type AuthPayload {
