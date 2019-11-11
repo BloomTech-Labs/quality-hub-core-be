40c1b29c3851de7ebe0df2e2899cd6ea234ae4b4
Jordan <jordandoan@hotmail.com>
2019-11-10 16:59:42 -0800

    Removed schema.graphql for schema.js, commented all utils and resolvers

--- src/resolvers/Industry.js
+++ src/resolvers/Industry.js
@@ -1,3 +1,10 @@
+
+
+/*
+  Gets users that are connected with its industry
+
+  @return {[Object]} of type User (look at ../schema.js)
+*/
 function users(parent, args, context, info) {
   return context.prisma.industry({ id: parent.id }).users() 
 }
