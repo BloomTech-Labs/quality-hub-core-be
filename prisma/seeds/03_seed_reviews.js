edd4c5304f1c2fce46698b69d83bf46339bdbbcb
Aaron Clark <aaronpclark92@gmail.com>
2020-01-30 15:19:02 -0600

    add function to seed user reviews from ResumeReview files, added script to execute said function, updated gitignore

--- prisma/seeds/03_seed_reviews.js
+++ prisma/seeds/03_seed_reviews.js
@@ -1,3 +1,55 @@
-// TODO - write functions to create reviews for InterviewQ services
+const { Prisma } = require('../../src/generated/prisma-client')
+const fs = require('fs')
+const path = require('path')
 
-// * check that ResumeReview is completed
+const db = new Prisma({
+  endpoint: process.env.PRISMA_ENDPOINT
+})
+
+const seeded_ResumeReviews = fs.readFileSync((path.resolve(__dirname, './seeded_resume_reviews.json'), (err => console.log(`err`))))
+
+const resumeReviews = JSON.parse(seeded_ResumeReviews)
+
+resumeReviews.filter(entry => {
+  entry.isComplete === true
+})
+
+// // TODO - write function to retrieve saved ResumeReviews 
+// seeded_resume_reviews.json
+// // TODO - write a function that filters out uncompleted ResumeReviews
+
+// TODO - write a function that takes a single ResumeReview, and passes an object to Prisma DB with the expected shape of a Review
+// * Review requires: coach, seeker, job_id, rating, 
+// * Optional for Review: review (text of review)
+
+const reviewText = [
+  'Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.',
+
+  'It is a way I have of driving off the spleen and regulating the circulation.Whenever I find myself growing grim about the mouth;',
+  'whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet;',
+  'and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then,',
+  'I account it high time to get to sea as soon as I can.',
+  'This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. '
+]
+
+async function createReview(resumeReview) {
+  // TODO add code to randomize rating
+  const rating = Math.ceil(Math.random() * 5);
+
+  const review = {
+    rating,
+    job_id: resumeReview.id,
+    coach: resumeReview.coach,
+    seeker: resumeReview.seeker,
+    review: reviewText[rating]
+  }
+
+  return await db.createReview(review)
+}
+
+
+if (process.env.PRISMA_ENDPOINT.includes('localhost')) {
+  resumeReviews.map(review => {
+    createReview(review)
+  })
+}
