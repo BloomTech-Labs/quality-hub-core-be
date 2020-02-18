c7501e192bef9602e1b865323a5d96a73139d93e
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-02-18 14:47:30 -0600

    Working auth0 token and verification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const express = require('express');
-const server = require('./server');
+require("dotenv").config();
+const express = require("express");
+const server = require("./server");
 
 // auth0
 const jwt = require("express-jwt");
@@ -8,23 +8,22 @@ const jwks = require("jwks-rsa");
 
 // middleware
 const authCheck = jwt({
-	secret: jwks.expressJwtSecret({
-	  cache: true,
-	  rateLimit: true,
-	  jwksRequestsPerMinute: 5,
-	  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-	}),
-	audience: [process.env.AUDIENCE],
-	issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-	algorithm: "RS256"
-  });
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+  audience: [process.env.AUDIENCE],
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithm: "RS256"
+});
 
-server.express.use('/doc', authCheck, express.static("doc/schema"));
+// server.express.use(authCheck);
+server.express.use("/doc", express.static("doc/schema"));
 
-const options = { port: (process.env.APOLLO_PORT || process.env.PORT) || 4444 };
-
-
+const options = { port: process.env.APOLLO_PORT || process.env.PORT || 4444 };
 
 server.start(options, ({ port }) => {
-	console.log(`QualityHub Core running on ${port}`);
+  console.log(`QualityHub Core running on ${port}`);
 });
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const formatKey = key => {
+  const beginKey = "-----BEGIN CERTIFICATE-----";
+  const endKey = "-----END CERTIFICATE-----";
+
+  const sanatizedKey = key
+    .replace(beginKey, "")
+    .replace(endKey, "")
+    .replace("\n", "");
+
+  const keyArray = sanatizedKey.split("").map((l, i) => {
+    const position = i + 1;
+    const isLastCharacter = sanatizedKey.length === position;
+    if (position % 64 === 0 || isLastCharacter) {
+      return l + "\n";
+    }
+    return l;
+  });
+
+  return `${beginKey}\n${keyArray.join("")}${endKey}\n`;
+};
 
 /*
   @param {Object} args - arguments sent into mutation such as fields for signup
@@ -12,7 +33,7 @@ const JWT_SECRET = process.env.JWT_SECRET
 function checkFields(args) {
   for (let key of Object.keys(args)) {
     if (!args[key]) {
-      throw new Error('Invalid input for required fields');
+      throw new Error("Invalid input for required fields");
     }
   }
 }
@@ -25,12 +46,12 @@ function checkFields(args) {
 function generateToken(user) {
   const payload = {
     id: user.id,
-    email: user.email,
+    email: user.email
   };
   const options = {
-    expiresIn: '3d'
-  }
-  return jwt.sign(payload, JWT_SECRET, options)
+    expiresIn: "3d"
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
 }
 
 /* 
@@ -42,61 +63,64 @@ function generateToken(user) {
   @return {ID} userId - User ID stored in token
 */
 function getUserId(context) {
-  // const Authorization = context.request.get('Authorization')
-  // if (Authorization) {
-  //   const token = Authorization.replace('Bearer ', '')
-  //   const { id: userId } = jwt.verify(token, JWT_SECRET)
-  //   return userId
-  // }
-  return 'ck6l9nbo4002r0904usi9aly2'
-  throw new Error('Not Authenticated')
+  const Authorization = context.request.get("Authorization");
+
+  if (Authorization) {
+    const token = Authorization.replace("Bearer ", "");
+    const userId = jwt.decode(token).sub;
+    console.log("GETTING USER ID: ", userId);
+    return userId;
+  }
+
+  // return "ck6qypadb000v0733r95a33gb";
+  throw new Error("Not Authenticated");
 }
 
 /*
   Checks the email stored in the token against the saved admin email in an .env file.
 */
 async function checkAdmin(context) {
-  const user = await context.prisma.user({ id: getUserId(context) })
+  const user = await context.prisma.user({ id: getUserId(context) });
   if (user.email !== process.env.ADMIN_EMAIL) {
-    throw new Error('You are unauthorized to perform this action.')
+    throw new Error("You are unauthorized to perform this action.");
   }
 }
 
 function validToken(context) {
-  const Authorization = context.request.get('Authorization')
+  const Authorization = context.request.get("Authorization");
   if (Authorization) {
-    const token = Authorization.replace('Bearer ', '')
+    const token = Authorization.replace("Bearer ", "");
     return jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
         return {
           token: "",
           valid: false
-        }
+        };
       } else {
         return {
           token: generateToken(decoded),
           valid: true
-        }
+        };
       }
-    })
+    });
   }
-  throw new Error('Not Authenticated')
+  throw new Error("Not Authenticated");
 }
 
 // this function is used to round average_coach_rating
 function round(value) {
-  console.log(`rounding value`, value)
+  console.log(`rounding value`, value);
   const rounded = Math.round(value * 2) / 2;
-  console.log(`rounded val`, rounded)
-  return rounded
+  console.log(`rounded val`, rounded);
+  return rounded;
 }
 
-
 module.exports = {
+  formatKey,
   checkFields,
   generateToken,
   getUserId,
   checkAdmin,
   validToken,
-  round,
-}
+  round
+};
